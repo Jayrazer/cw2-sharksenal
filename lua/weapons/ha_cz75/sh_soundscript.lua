06ef5b88221c864759b4dbe1ee2db642c532660f
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-05-28 22:46:15 -0400

    Update sh_soundscript.lua

--- lua/weapons/ha_cz75/sh_soundscript.lua
+++ lua/weapons/ha_cz75/sh_soundscript.lua
@@ -30,26 +30,26 @@ SWEP.Sounds = {
 	
 	reload = {
 		{time = 1/31.5, sound = "CZ75_MAGOUT"},
-		{time = 35/31.5, sound = "CZ75_MAGIN"},
+		{time = 33/31.5, sound = "CZ75_MAGIN"},
 		{time = 38/30, sound = "CW_FOLEY_MEDIUM"},
 	},
 
 	reload_empty = {
 		{time = 3/31.5, sound = "CZ75_MAGOUT"},
-		{time = 35/31.5, sound = "CZ75_MAGIN"},
+		{time = 34/31.5, sound = "CZ75_MAGIN"},
 		{time = 51/31.5, sound = "CZ75_SLIDERELEASE"},
 		{time = 60/31, sound = "CW_FOLEY_MEDIUM"},
 	},
 	
 	reload_auto = {
 		{time = 1/31.5, sound = "CZ75_MAGOUT"},
-		{time = 35/31.5, sound = "CZ75_MAGIN"},
+		{time = 33/31.5, sound = "CZ75_MAGIN"},
 		{time = 38/30, sound = "CW_FOLEY_MEDIUM"},
 	},
 
 	reload_empty_auto = {
 		{time = 3/31.5, sound = "CZ75_MAGOUT"},
-		{time = 35/31.5, sound = "CZ75_MAGIN"},
+		{time = 34/31.5, sound = "CZ75_MAGIN"},
 		{time = 57/31.5, sound = "CZ75_SLIDEBACK"},
 		{time = 59/31.5, sound = "CZ75_SLIDERELEASE"},
 		{time = 65/31, sound = "CW_FOLEY_MEDIUM"},
