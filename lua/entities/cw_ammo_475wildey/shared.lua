9ab15442e0f62fb76ff1cd403ce0b1ac56159dc7
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-05-31 11:10:54 -0400

    475 ammo ent

---
+++ lua/entities/cw_ammo_475wildey/shared.lua
@@ -0,0 +1,13 @@
+ENT.Type = "anim"
+ENT.Base = "cw_ammo_ent_base"
+ENT.PrintName = ".475 Wildey Ammo"
+ENT.Author = "speedonerd"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true 
+ENT.Category = "CW 2.0 Sharksenal Ammo"
+
+ENT.CaliberSpecific = true
+ENT.AmmoCapacity = 40
+ENT.ResupplyAmount = 10
+ENT.Caliber = ".475 Wildey Magnum"
+ENT.Model = "models/Items/BoxMRounds.mdl"
