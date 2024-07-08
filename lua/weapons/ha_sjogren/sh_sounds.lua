eebb2276856a1b7a9f9359c9bdaadb6bc6937dbd
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-07-08 10:18:18 -0400

    sjogren's gone

--- lua/weapons/ha_sjogren/sh_sounds.lua
+++
@@ -1,7 +0,0 @@
-CustomizableWeaponry:addFireSound("SJOGREN_FIRE", "^weapons/cw2_shark/sjogren/shoot.wav", 1, 100, CHAN_STATIC)
-
-CustomizableWeaponry:addReloadSound("SJOGREN_BOLTBACK", "weapons/cw2_shark/sjogren/firemech.wav")
-CustomizableWeaponry:addReloadSound("SJOGREN_BOLTFORWARD", "weapons/cw2_shark/sjogren/boltforward.wav")
-CustomizableWeaponry:addReloadSound("SJOGREN_INSERT", "weapons/cw2_shark/sjogren/insert.wav")
-CustomizableWeaponry:addReloadSound("SJOGREN_INSERT_EMPTY", "weapons/cw2_shark/sjogren/insert_e.wav")
-CustomizableWeaponry:addReloadSound("SJOGREN_DEPLOY", "weapons/cw2_shark/sjogren/cloth.wav")
